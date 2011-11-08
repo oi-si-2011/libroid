4a70884c9e1dc4998a30fa9f98dd52b1c7ed8da0
Petr Messner <petr.messner@gmail.com>
2011-11-09 00:25:18 +0100

    zakladni kostra serializace do XML

--- test/libroid/model/SerializationTest.java
+++ test/libroid/model/SerializationTest.java
@@ -1,23 +1,29 @@
 package libroid.model;
 
+import java.io.UnsupportedEncodingException;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class SerializationTest {
 
     @Test
-    public void testSerializeEmptyModel() {
+    public void testSerializeEmptyModel() throws UnsupportedEncodingException {
         Model m = new Model();
-        String result = Serialization.serialize(m);
-        assertEquals("??", result);
+        byte[] result = Serialization.serialize(m);
+        String resultStr = new String(result, "latin1");
+        //System.out.println("Result: '" + resultStr + "'");
+        String expected = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n\n";
+        assertEquals(expected, resultStr);
     }
 
     @Test
-    public void testSerializeSampleModel() {
+    public void testSerializeSampleModel() throws UnsupportedEncodingException {
         Model m = new Model();
         m.addBook(new Book("Vlakna hypercasu", "R. Susta"));
         m.addBook(new Book("Kryptonomikon", "N. Stephenson"));
-        String result = Serialization.serialize(m);
-        assertEquals("??", result);
+        byte[] result = Serialization.serialize(m);
+        String resultStr = new String(result, "latin1");
+        String expected = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n\n";
+        assertEquals(expected, resultStr);
     }
 }
