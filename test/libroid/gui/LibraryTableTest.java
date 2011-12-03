762011fd0d17cf36d0111b3b933beefe7527a670
Mymicky <vrbami@seznam.cz>
2011-12-03 22:36:27 +0100

    unit testy

---
+++ test/libroid/gui/LibraryTableTest.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package libroid.gui;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Michal
+ */
+public class LibraryTableTest {
+    
+    public LibraryTableTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+}
